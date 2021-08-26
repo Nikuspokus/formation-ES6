9ce591a75c0fb761403799e797a437e7b65b4de2
NICOLAS MARQUILLIER <10154106@leroymerlin.fr>
2021-08-26 15:58:21 +0200

    section 2 - chap 19

--- main.js
+++ main.js
@@ -4,11 +4,9 @@
 // var num = 1;
 // var chiffre;
 // var chiffre2 = 2;
-
 // console.log(num);
 // console.log(chiffre);
 // console.log(chiffre2 + num);
-
 ////////////////////////////////////
 // Section 2 - chap 7
 ////////////////////////////////////
@@ -76,4 +74,50 @@
 // Section 2 - chap 15
 ////////////////////////////////////
 // Echapper un caractère
-
+// retour à la ligne
+// var texte = "Coucou \nNico "
+// console.log(texte);
+// ajouter une tabulation
+// var texte1 = "Coucou \tNico "
+// console.log(texte1);
+// ajouter un backslash
+// var texte2 = "Coucou \\Nico "
+// console.log(texte2);
+////////////////////////////////////
+// Section 2 - chap 16
+////////////////////////////////////
+// Concaténer des chaînes de caractères
+// var bonjour = "Bonjour"
+// var nom = "Nicolas"
+// console.log(bonjour + ' ' + nom);
+// var resultat = bonjour + ' ' + nom 
+// console.log(resultat);
+// bonjour += ' ' + nom
+// console.log(bonjour);
+////////////////////////////////////
+// Section 2 - chap 17
+////////////////////////////////////
+// Exercice pratique générer une phrase
+// var bonjour = "bonjour"
+// var nom = "Nicolas"
+// var as ="tu as :"
+// var age = 35
+// var resultat = "Bonjour " + nom + " tu as " + age + " ans !"
+// console.log(bonjour + " " + nom + " " + as + " " + age);
+// console.log(resultat);
+////////////////////////////////////
+// Section 2 - chap 18
+////////////////////////////////////
+// trouver la taille d'une chaîne de caractère
+// var nom = "Nicolas"
+// console.log(nom.length);
+// console.log("nom".length);
+// console.log(nom[2]);
+// console.log(nom[nom.length - 2]);
+////////////////////////////////////
+// Section 2 - chap 19
+////////////////////////////////////
+// l'immutabilité
+var nom = "Nicolaz"
+nom[nom.length - 1] = "s"
+console.log(nom[nom.length - 1]);
